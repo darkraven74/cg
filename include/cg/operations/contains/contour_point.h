6da54a39271b1dfe2839f1dfe60712cc3d089588
Vladimir Baev <darkraven8@gmail.com>
2013-04-26 19:45:31 +0400

    convex contour contains point with tests added

--- include/cg/operations/contains/contour_point.h
+++ include/cg/operations/contains/contour_point.h
@@ -11,18 +11,25 @@ namespace cg
    template<class Scalar>
    bool contains_convex(contour_2t<Scalar> const & c, point_2t<Scalar> const & q)
    {
-      if (c.vertices_num() == 0)
+      switch (c.vertices_num())
       {
-         return false;
+      case 0: return false;
+      case 1: return c[0] == q;
+      case 2: return contains(segment_2t<Scalar>(c[0], c[1]), q);
       }
-      if (c.vertices_num() == 1)
-      {
-         return (c[0] == q);
-      }
-      if (c.vertices_num() == 2)
-      {
-         return (contains(segment_2t<Scalar>(c[0], c[1]), q));
-      }
-      //TODO: ...
+
+      contour_2::const_iterator it = std::lower_bound(c.begin() + 2, c.end(), q,
+                       [&c] (point_2 const& a, point_2 const& b)
+                              {
+                                                      return orientation(c[0], a, b) == CG_LEFT;
+                              }
+                                                     );
+   if (it == c.end())
+   {
+      return false;
+   }
+
+   return (orientation(*(it - 1), *it, q) != CG_RIGHT);
+
    }
 }
--- tests/contains.cpp
+++ tests/contains.cpp
@@ -1,7 +1,8 @@
 #include <gtest/gtest.h>
-
+#include <boost/assign/list_of.hpp>
 #include <cg/operations/contains/segment_point.h>
 #include <cg/operations/contains/triangle_point.h>
+#include <cg/operations/contains/contour_point.h>
 
 TEST(contains, triangle_point)
 {
@@ -60,4 +61,51 @@ TEST(contains, segment_point)
 
    EXPECT_FALSE(cg::contains(s, point_2(1, 0)));
    EXPECT_FALSE(cg::contains(s, point_2(0, 1)));
+}
+
+TEST(contains, convex_contour_point)
+{
+   using cg::point_2;
+
+   std::vector<point_2> pts = boost::assign::list_of(point_2(0, 0));
+
+
+   EXPECT_TRUE(cg::contains_convex(cg::contour_2(pts), cg::point_2(0, 0)));
+   EXPECT_FALSE(cg::contains_convex(cg::contour_2(pts), cg::point_2(0, 5)));
+}
+
+TEST(contains, convex_contour_point2)
+{
+   using cg::point_2;
+
+   std::vector<point_2> pts = boost::assign::list_of(point_2(0, 0))
+                                                    (point_2(5, 0));
+
+
+   EXPECT_TRUE(cg::contains_convex(cg::contour_2(pts), cg::point_2(0, 0)));
+   EXPECT_TRUE(cg::contains_convex(cg::contour_2(pts), cg::point_2(3, 0)));
+   EXPECT_TRUE(cg::contains_convex(cg::contour_2(pts), cg::point_2(5, 0)));
+   EXPECT_FALSE(cg::contains_convex(cg::contour_2(pts), cg::point_2(2, 2)));
+   EXPECT_FALSE(cg::contains_convex(cg::contour_2(pts), cg::point_2(3, 3)));
+   EXPECT_FALSE(cg::contains_convex(cg::contour_2(pts), cg::point_2(4, 4)));
+}
+
+TEST(contains, convex_contour_point3)
+{
+   using cg::point_2;
+
+   std::vector<point_2> pts = boost::assign::list_of(point_2(0, 0))
+                                                    (point_2(5, 0))
+                                                    (point_2(5, 5))
+                                                    (point_2(0, 5));
+
+   EXPECT_TRUE(cg::contains_convex(cg::contour_2(pts), cg::point_2(0, 0)));
+   EXPECT_TRUE(cg::contains_convex(cg::contour_2(pts), cg::point_2(0, 5)));
+   EXPECT_TRUE(cg::contains_convex(cg::contour_2(pts), cg::point_2(2, 2)));
+   EXPECT_TRUE(cg::contains_convex(cg::contour_2(pts), cg::point_2(4, 4)));
+   EXPECT_TRUE(cg::contains_convex(cg::contour_2(pts), cg::point_2(4, 5)));
+   EXPECT_TRUE(cg::contains_convex(cg::contour_2(pts), cg::point_2(0, 2)));
+   EXPECT_FALSE(cg::contains_convex(cg::contour_2(pts), cg::point_2(6, 0)));
+   EXPECT_FALSE(cg::contains_convex(cg::contour_2(pts), cg::point_2(6, 6)));
+   EXPECT_FALSE(cg::contains_convex(cg::contour_2(pts), cg::point_2(10, 0)));
 }
